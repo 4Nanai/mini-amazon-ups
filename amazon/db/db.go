d2e01e41c508b7190ebc1e19f2260584f9161573
chitose <103087652@qq.com>
2026-02-20 11:32:51 -0500

    feat(database): define Warehouse struct and implement InitWarehouses method

--- amazon/db/db.go
+++ amazon/db/db.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"mini-amazon-ups/proto"
 	"os"
+
+	protobuf "google.golang.org/protobuf/proto"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,6 +12,12 @@ import (
 
 type Database struct {
 	db *gorm.DB
+}
+
+type Warehouse struct {
+	WarehouseID int `gorm:"primaryKey;autoIncrement"`
+	X           int `gorm:"not null"`
+	Y           int `gorm:"not null"`
 }
 
 func NewDatabase() *Database {
@@ -21,4 +30,24 @@ func NewDatabase() *Database {
 		panic("failed to connect database")
 	}
 	return &Database{db: db}
+}
+
+func (d *Database) InitWarehouses(count int) ([]*proto.AInitWarehouse, error) {
+	warehouses := make([]Warehouse, 0, count)
+	for i := 1; i <= count; i++ {
+		warehouses = append(warehouses, Warehouse{
+			X: i * 10,
+			Y: i * 10,
+		})
+	}
+	d.db.Create(warehouses)
+	initWarehouses := make([]*proto.AInitWarehouse, 0, count)
+	for _, warehouse := range warehouses {
+		initWarehouses = append(initWarehouses, &proto.AInitWarehouse{
+			Id: protobuf.Int32(int32(warehouse.WarehouseID)),
+			X:  protobuf.Int32(int32(warehouse.X)),
+			Y:  protobuf.Int32(int32(warehouse.Y)),
+		})
+	}
+	return initWarehouses, nil
 }
